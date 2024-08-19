dcc1a266eb7dc96767cda15eff24ff74b8e62738
marcelog5 <marcelovictorg2@gmail.com>
2024-08-19 02:48:40 -0300

    Create chat entity

---
+++ DateABot/Domain/Chats/Chat.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Abstracts;
+
+namespace Domain.Chats
+{
+    public sealed class Chat : Entity
+    {
+        public Chat(
+            Guid userId, 
+            Guid botId)
+        {
+            if (userId == Guid.Empty)
+            {
+                throw new Exception("User id is required");
+            }
+            if (botId == Guid.Empty)
+            {
+                throw new Exception("Bot id is required");
+            }
+
+            UserId = userId;
+            BotId = botId;
+        }
+
+        public Guid UserId { get; private set; }
+        public Guid BotId { get; private set; }
+    }
+}
---
+++ DateABot/Test/Domain/ChatTests.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Chats;
+
+namespace Test.Domain
+{
+    public class ChatTests
+    {
+        [Fact]
+        public void Create_WhenCalledWithValidParameters_CreatesChat()
+        {
+            // Arrange
+            var validUserId = Guid.NewGuid();
+            var validBotId = Guid.NewGuid();
+
+            // Act
+            var chat = new Chat(validUserId, validBotId);
+
+            // Assert
+            Assert.NotNull(chat);
+            Assert.Equal(validUserId, chat.UserId);
+            Assert.Equal(validBotId, chat.BotId);
+        }
+
+        [Fact]
+        public void Create_WhenCalledWithEmptyUserId_ThrowsException()
+        {
+            // Arrange
+            var emptyUserId = Guid.Empty;
+            var validBotId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => new Chat(emptyUserId, validBotId));
+        }
+
+        [Fact]
+        public void Create_WhenCalledWithEmptyBotId_ThrowsException()
+        {
+            // Arrange
+            var validUserId = Guid.NewGuid();
+            var emptyBotId = Guid.Empty;
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => new Chat(validUserId, emptyBotId));
+        }
+
+        [Fact]
+        public void Create_WhenCalledWithEmptyUserIdAndBotId_ThrowsException()
+        {
+            // Arrange
+            var emptyUserId = Guid.Empty;
+            var emptyBotId = Guid.Empty;
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => new Chat(emptyUserId, emptyBotId));
+        }
+    }
+}
